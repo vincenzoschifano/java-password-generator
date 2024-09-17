dbd44fe4f02dd39dc127061296638a3f35c0f405
vincenzoschifano <vincenzoschifano94@gmail.com>
2024-09-18 00:32:12 +0200

    ADD generator

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+
+
+	public static void main(String[] args) {
+		
+		String name = "Luca";
+		String surname = "Salvadori";
+		String colour = "Senape";
+		int birthDay = 31;
+		int birthMouth = 12;
+		int birthYear = 1980;
+		int summ = 31 + 12 + 1980;
+		
+		
+		System.out.println("La sua password sarà " + name + "-" + surname + "-" + summ );
+				
+		
+		
+	}
+}
